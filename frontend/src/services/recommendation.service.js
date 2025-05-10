627852923c951bb69dc0aba1ff222d6f5f7c6b71
Beatriz Gonçalves <biagonz@live.com>
2025-05-10 20:09:21 -0300

    feat: implement getRecommendations function

--- frontend/src/services/recommendation.service.js
+++ frontend/src/services/recommendation.service.js
@@ -1,12 +1,56 @@
 // getRecommendations.js
 
 const getRecommendations = (
-  formData = { selectedPreferences: [], selectedFeatures: [] },
+  formData = {
+    selectedPreferences: [],
+    selectedFeatures: [],
+    selectedRecommendationType: 'MultipleProducts',
+  },
   products
 ) => {
-  /**
-   * Crie aqui a lógica para retornar os produtos recomendados.
-   */
+  const { selectedPreferences, selectedFeatures, selectedRecommendationType } =
+    formData;
+
+  const calculateItemsSelected = (product) => {
+    const preferences = product.preferences.filter((preference) =>
+      selectedPreferences?.includes(preference)
+    );
+    const features = product.features.filter((feature) =>
+      selectedFeatures?.includes(feature)
+    );
+
+    return preferences.length + features.length;
+  };
+
+  const filteredProducts = products
+    .map((product, index) => ({
+      ...product,
+      itemsSelected: calculateItemsSelected(product),
+      position: index,
+    }))
+    .filter((product) => product.itemsSelected > 0);
+
+  if (selectedRecommendationType === 'SingleProduct') {
+    const sortedProducts = Array.from(filteredProducts).sort(
+      (a, b) => b.itemsSelected - a.itemsSelected
+    );
+
+    const firstPosition = sortedProducts[0];
+    const secondPosition = sortedProducts[1];
+
+    const hasBreakEven =
+      firstPosition?.itemsSelected === secondPosition?.itemsSelected;
+
+    const item = hasBreakEven ? secondPosition : firstPosition;
+
+    return [item];
+  }
+
+  return filteredProducts;
 };
 
-export default { getRecommendations };
+const recommendationsService = {
+  getRecommendations,
+};
+
+export default recommendationsService;
